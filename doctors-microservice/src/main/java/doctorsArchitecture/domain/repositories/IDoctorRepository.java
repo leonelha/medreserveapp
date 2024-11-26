75ca95f76d7160e39f1a1c0836a251d6960fa7fb
Cristian220609 <143531896+Cristian220609@users.noreply.github.com>
2024-11-26 15:31:29 -0500

    Delete doctors-microservice directory

--- doctors-microservice/src/main/java/doctorsArchitecture/domain/repositories/IDoctorRepository.java
+++
@@ -1,7 +0,0 @@
-package doctorsArchitecture.domain.repositories;
-
-import doctorsArchitecture.domain.models.Doctor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IDoctorRepository extends JpaRepository<Doctor, Long> {
-}
