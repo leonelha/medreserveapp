3fef85bfafd0de85fe23f4cad75daf97850b0346
mauriav04 <virtuspro65@gmail.com>
2024-11-26 00:37:24 -0500

    ReserveClinicController actualizacion

--- reserveClinic/microservices/client-service/src/test/java/com/reserveclinic/client_service/ClientServiceApplicationTests.java
+++ reserveClinic/microservices/client-service/src/test/java/com/reserveclinic/client_service/ClientServiceApplicationTests.java
@@ -1,13 +1,25 @@
-package com.reserveclinic.client_service;
+@RestController
+@RequestMapping("/api/reserveClinic")
+public class ReserveClinicController {
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+	@Autowired
+	private ReserveClinicService reserveClinicService;
 
-@SpringBootTest
-class ClientServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
+	// Endpoint para realizar una reserva
+	@PostMapping("/reserve")
+	public ResponseEntity<String> reserveClinic(@RequestBody ReserveClinicRequest request) {
+		boolean success = reserveClinicService.createReservation(request);
+		if (success) {
+			return ResponseEntity.ok("Reserva realizada con éxito.");
+		} else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error al realizar la reserva.");
+		}
 	}
 
+	// Endpoint para obtener todas las reservas
+	@GetMapping("/reservations")
+	public ResponseEntity<List<ReserveClinic>> getAllReservations() {
+		List<ReserveClinic> reservations = reserveClinicService.getAllReservations();
+		return ResponseEntity.ok(reservations);
+	}
 }
