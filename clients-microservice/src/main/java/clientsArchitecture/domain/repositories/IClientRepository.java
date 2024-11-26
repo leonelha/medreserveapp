f18679c6251fc840bc4299885ad3311a19de85a2
Cristian220609 <143531896+Cristian220609@users.noreply.github.com>
2024-11-26 15:31:17 -0500

    Delete clients-microservice directory

--- clients-microservice/src/main/java/clientsArchitecture/domain/repositories/IClientRepository.java
+++
@@ -1,7 +0,0 @@
-package clientsArchitecture.domain.repositories;
-
-import clientsArchitecture.domain.models.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IClientRepository extends JpaRepository<Client, Long> {
-}
